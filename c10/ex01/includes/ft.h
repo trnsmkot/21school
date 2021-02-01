d9cbbf6609b43a538f6e2e0382bd9a600941f84e
Vladislav Kasatkin <vlad.n.kasatkin@gmail.com>
2021-02-01 18:42:01 +0300

    Add c10 ex01

---
+++ c10/ex01/includes/ft.h
@@ -0,0 +1,12 @@
+#ifndef FT_H
+# define FT_H
+# define BYTE_COUNT 255
+
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+void	ft_fill_str(char *str, int size);
+void	ft_put_substr(char *str, int size);
+
+#endif
---
+++ c10/ex01/srcs/main.c
@@ -0,0 +1,29 @@
+#include "../includes/ft.h"
+#include <fcntl.h>
+#include <zconf.h>
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	char buffer[BYTE_COUNT];
+	int fd;
+	int read_byte;
+
+	if (argc < 2)
+		ft_putstr("File name missing.\n");
+	else if (argc > 2)
+		ft_putstr("Too many arguments.\n");
+	else
+	{
+		ft_fill_str(buffer, BYTE_COUNT);
+		if ((fd = open(argv[1], O_RDONLY)) == -1)
+		{
+			ft_putstr("Cannot read file.\n");
+			return (0);
+		}
+		while ((read_byte = read(fd, buffer, BYTE_COUNT)) != 0)
+			ft_put_substr(buffer, read_byte);
+		close(fd);
+	}
+	return (0);
+}
