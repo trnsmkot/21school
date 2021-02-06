7b8768ada35e5f6ad0c5db0fe56ff9b8800d7b8e
Vladislav Kasatkin <vlad.n.kasatkin@gmail.com>
2021-02-06 11:06:34 +0300

    Init BSQ

---
+++ BSQ/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// open, close, read, write, malloc, free, exit
+int main()
+{
+	printf("Hello, World!\n");
+	return 0;
+}
---
+++ BSQ/map_generator.pl
@@ -0,0 +1,18 @@
+# perl map_generator.pl 10 10 5 > map.txt
+#########################################
+#!/usr/bin/perl
+use warnings;
+use strict;
+die "program x y density" unless (scalar(@ARGV) == 3);
+my ($x, $y, $density) = @ARGV;
+print "$y.ox\n";
+for (my $i = 0; $i < $y; $i++) {
+  for (my $j = 0; $j < $x; $j++) {
+    if (int(rand($y) * 2) < $density) {
+      print "o";
+    } else {
+      print ".";
+    }
+  }
+  print "\n";
+}
